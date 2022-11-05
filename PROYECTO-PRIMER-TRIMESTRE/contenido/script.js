ee2642f197ec3cb1a84bc63bf8185dd627c1629b
ramekiano <alraga90@gmail.com>
2022-11-05 17:32:40 +0100

    he añadido todos las preguntas de colores

--- PROYECTO-PRIMER-TRIMESTRE/contenido/script.js
+++ PROYECTO-PRIMER-TRIMESTRE/contenido/script.js
@@ -107,28 +107,37 @@ function iniciar() {
             case 1: case 2: case 3: case 10: case 11: case 123: case 143:
                 nodo.setAttribute("color", "verde");
                 break;
-            case 4: case 6: case 26: case 37: case 46: case 47: case 58: case 59: case 98: case 99:
+            case 4: case 6: case 26: case 37: case 46: case 47: case 58: case 59: case 98: case 99: case 136: case 146: case 149:
                 nodo.setAttribute("color", "naranja");
                 break;
-            case 5:
+            case 5: case 100: case 124: case 129:
                 nodo.setAttribute("color", "rojo");
                 break;
-            case 7: case 8: case 9: case 29: case 30: case 31:
+            case 7: case 8: case 9: case 29: case 30: case 31: case 41: case 42: case 43: case 44: case 45: case 55: case 60: case 61: case 62: case 72: case 73: case 114: case 116: case 117: case 130: case 131: case 134: case 138: case 139: case 144: case 147: case 148:
                 nodo.setAttribute("color", "azul");
                 break;
-            case 12: case 19: case 23: case 24: case 32: case 33: case 34:
+            case 12: case 19: case 23: case 24: case 32: case 33: case 34: case 48: case 49: case 88: case 89: case 90: case 91: case 92: case 93: case 94: case 109: case 110: case 121:
                 nodo.setAttribute("color","morado");
                 break;
-            case 13: case 20: case 21: case 22:
+            case 13: case 20: case 21: case 22: case 50: case 51: case 84: case 85: case 103: case 104: case 105: case 106: case 107: case 115: case 120: case 127: case 128: case 133: case 140: case 141:
                 nodo.setAttribute("color","marron");
                 break;
-            case 14: case 15: case 25: case 27: case 28:
+            case 14: case 15: case 25: case 27: case 28: case 54: case 63: case 64: case 65: case 69: case 70: case 71: case 96: case 97: case 125: case 126: case 135: case 145:
                 nodo.setAttribute("color","amarillo");
                 break;
-            case 16: case 17: case 18:
-                nodo.setAttribute("color","beige");
+            case 16: case 17: case 18: case 38: case 52: case 53: case 56: case 57: case 77: case 78: case 83: case 86: case 87: case 101: case 118:
+                nodo.setAttribute("color","blanco/beige");
+                break;
+            case 35: case 36: case 39: case 40: case 79: case 80: case 102: case 108: case 113: case 119: case 122: case 132: case 137: case 151:
+                nodo.setAttribute("color","rosa");
+                break;
+            case 66: case 67: case 68: case 74: case 75: case 76: case 81: case 82: case 95: case 111: case 112: case 142: case 150:
+                nodo.setAttribute("color","gris");
                 break;
             /*Nos quedamos en  Clefairy*/
+            /*que hacemos con voltorb y electrode¿? los pongo de momento voltorb rojo y el electrode blanco¿? (nº100 y 101)*/ 
+            /*He puesto el blanco y beige juntos porque quita de posibles dudas*/
+            /*Hitmonchan (nº107 lo he puesto marrón por su piel, pero puede ser morado)*/
         }
         caja.appendChild(nodo);
         document.getElementById("tabla").appendChild(caja);
@@ -157,6 +166,9 @@ function boton() {
             break;
         case "evos":
             evaluarEvos();
+            break;
+        case "colors":
+            evaluarColors();
             break;
     }
     intentos--;
@@ -246,4 +258,29 @@ function evaluarEvos(){
         document.getElementById("pistas").appendChild(texto);
     }
     document.getElementById(document.getElementsByClassName("evos")[0].value).remove();
+}
+
+function evaluarColors(){
+    if (document.getElementsByClassName("colors")[0].value == seleccion.getAttribute("color")){
+        for (i = 0; i < 151; i++) {
+            if (document.getElementsByClassName("pokemon")[i].getAttribute("color") != document.getElementsByClassName("colors")[0].value) {
+                document.getElementsByClassName("pokemon")[i].src="pokibol.PNG";
+                document.getElementsByClassName("pokemon")[i].style.animation="guardar 2s";
+            }
+        }
+        texto=document.createElement("p");
+        texto.innerHTML=`Es de color: ${document.getElementsByClassName("colors")[0].value}`;
+        document.getElementById("pistas").appendChild(texto);
+    }else{
+        for (i = 0; i < 151; i++) {
+            if (document.getElementsByClassName("pokemon")[i].getAttribute("color") == document.getElementsByClassName("colors")[0].value) {
+                document.getElementsByClassName("pokemon")[i].src="pokibol.PNG";
+                document.getElementsByClassName("pokemon")[i].style.animation="guardar 2s";
+            }
+        }
+        texto=document.createElement("p");
+        texto.innerHTML=`No es de color: ${document.getElementsByClassName("colors")[0].value}`;
+        document.getElementById("pistas").appendChild(texto);
+    }
+    document.getElementById(document.getElementsByClassName("colors")[0].value).remove();
 }
